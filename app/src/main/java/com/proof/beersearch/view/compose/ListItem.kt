025a74ffb031484a631b7ef8bcfccbb932c0bb3e
Susana Cordoba <akimasc@gmail.com>
2023-05-12 14:39:18 +0200

    no message

--- app/src/main/java/com/proof/beersearch/view/compose/ListItem.kt
+++ app/src/main/java/com/proof/beersearch/view/compose/ListItem.kt
@@ -1,6 +1,5 @@
 package com.proof.beersearch.view.compose
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
@@ -12,7 +11,6 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import coil.annotation.ExperimentalCoilApi
